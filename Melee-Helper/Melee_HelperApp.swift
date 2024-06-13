c4a901f58aa9ecd73ed5b76fbaacdcd1d4bb33d4
hjaver11 <hejavery@gmail.com>
2024-06-13 10:33:18 -0700

    Initial Commit

---
+++ Melee-Helper/Melee_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Melee_HelperApp.swift
+//  Melee-Helper
+//
+//  Created by Harrison Javery on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Melee_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
