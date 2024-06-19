07715f19f0c80443787ef922a279750ece9f478c
hjaver11 <hejavery@gmail.com>
2024-06-18 19:51:50 -0700

    got environment variables for open AI api keys to be dopne correctly this time. Before was using environment xcode vartiables and didnt realize that doessnt apply to outside xcode builds. So added a way to add it to the info.plist, and now read it from there

--- Melee-Helper/Utilities/OpenAIService.swift
+++ Melee-Helper/Utilities/OpenAIService.swift
@@ -10,54 +10,44 @@ import OpenAI
 import Combine
 
 class OpenAIService: ObservableObject {
-    private var openAI: OpenAI
+    private var openAI: OpenAI?
 
     init() {
-        // Access environment variables
-        guard let apiKey = ProcessInfo.processInfo.environment["OPENAI_API_KEY"],
-              let organization = ProcessInfo.processInfo.environment["OPENAI_ORGANIZATION"] else {
-            fatalError("Environment variables not set")
+        do {
+            try initializeOpenAI()
+        } catch {
+            print("Failed to initialize OpenAI: \(error.localizedDescription)")
+            // Handle initialization error appropriately, such as setting a flag or notifying the user
+        }
+    }
+    
+    private func initializeOpenAI() throws {
+        // Access environment variables for debug in xcode
+//        guard let apiKey = ProcessInfo.processInfo.environment["OPENAI_API_KEY"],
+//              let organization = ProcessInfo.processInfo.environment["OPENAI_ORGANIZATION"] else {
+//            throw InitializationError.missingEnvironmentVariables
+//        }
+      
+        guard let apiKey = Bundle.main.infoDictionary?["OPENAI_API_KEY"],
+              let organization = Bundle.main.infoDictionary?["OPENAI_ORGANIZATION"] else {
+            throw InitializationError.missingEnvironmentVariables
         }
 
+        
         // Initialize OpenAI client
-        let configuration = OpenAI.Configuration(token: apiKey, organizationIdentifier: organization, timeoutInterval: 60.0)
+        let configuration = OpenAI.Configuration(token: apiKey as! String, organizationIdentifier: organization as? String, timeoutInterval: 60.0)
         self.openAI = OpenAI(configuration: configuration)
     }
-
-    // Function to fetch a completion from OpenAI using async/await
-//    func fetchChatCompletion(systemPrompt: String, userPrompt: String, completed: @escaping (Result<String,Error>)-> Void) async {
-//        let userMessage = ChatQuery.ChatCompletionMessageParam.user(.init(content: .string(userPrompt)))
-//        let systemMessage = ChatQuery.ChatCompletionMessageParam.system(.init(content: systemPrompt))
-//           let query = ChatQuery(
-//               messages: [systemMessage,userMessage],
-//               model: .gpt4_o,
-//               frequencyPenalty: 1,  // Penalize repetition
-//               maxTokens: 1000,
-//               presencePenalty: 1,  // Encourage new topics
-//               temperature: 1,  // Increase creativity
-//               topP: 0.8  // Consider more diverse options
-//           )
-//           
-//           do {
-//               let result = try await openAI.chats(query: query)
-//               if let completion = result.choices.first?.message.content?.string {
-//                   DispatchQueue.main.async {
-//                       completed(.success(completion))
-//                   }
-//               }
-//           } catch {
-//               DispatchQueue.main.async {
-//                   completed(.failure(error))
-//               }
-//           }
-//       }
-    
     
     func fetchChatCompletion(systemPrompt: String, userPrompt: String) async throws -> String {
+        guard let openAI = openAI else {
+            throw NSError(domain: "OpenAIErrorDomain", code: 1, userInfo: [NSLocalizedDescriptionKey: "OpenAI client not initialized"])
+        }
+
         let userMessage = ChatQuery.ChatCompletionMessageParam.user(.init(content: .string(userPrompt)))
         let systemMessage = ChatQuery.ChatCompletionMessageParam.system(.init(content: systemPrompt))
         let query = ChatQuery(
-            messages: [systemMessage,userMessage],
+            messages: [systemMessage, userMessage],
             model: .gpt4_o,
             frequencyPenalty: 1,  // Penalize repetition
             maxTokens: 1000,
@@ -72,11 +62,19 @@ class OpenAIService: ObservableObject {
                 throw NSError(domain: "OpenAIErrorDomain", code: 0, userInfo: [NSLocalizedDescriptionKey: "No content found in response"])
             }
             return finalString
-            
         } catch {
             throw error
         }
+    }
+    
+    enum InitializationError: Error, LocalizedError {
+        case missingEnvironmentVariables
         
+        var errorDescription: String? {
+            switch self {
+            case .missingEnvironmentVariables:
+                return NSLocalizedString("Required environment variables are missing.", comment: "Missing Environment Variables")
+            }
+        }
     }
 }
-
